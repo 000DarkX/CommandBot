5488cb7b612a9475296745993723c95be1a0738e
000DarkX <000DarkX@users.noreply.github.com>
2025-10-30 01:15:50 -0400

    Add files via upload

--- modules/basics/module.mjs
+++ modules/basics/module.mjs
@@ -1,6 +1,79 @@
 
 
 export const name = "Basics";
+
+const schema = {
+            "Say": { 
+                args: { text: { type: "string", default: "Hello" } }, 
+                subWorkspace: false,
+                action: (cmd, unit, state) => {
+                    log("Say: " + (cmd.args.text || ""));
+                }
+            },
+            "Feel": { 
+                args: { space: { type: ["Empty","Wall","Stairs","Unit"], default: "Empty" } }, 
+                subWorkspace: true,
+                action: (cmd, unit, state) => {
+                    log("Feel: " + (cmd.args.space || ""));
+                    exec.call(this, cmd.workspace);
+                }
+            },
+            "Hear": { 
+                args: { phrase: { type: "string", default: "Hi" } }, 
+                subWorkspace: true,
+                action: function (cmd, unit, state) {
+                    log("Hear: " + (cmd.args.phrase || ""));
+                    exec.call(this, cmd.workspace);
+                }
+            },
+            "Walk": { 
+                args: { steps: { type: "number", default: 1 } }, 
+                subWorkspace: false,
+                action: function (cmd, unit, state) {
+                    let steps = parseInt(cmd.args.steps) || 0;
+                    for (let i=0; i<steps; i++) {
+                        if (unit.dir===0 && unit.y>0) unit.y--;
+                        if (unit.dir===1 && unit.x + 1 < this.map.width) unit.x++;
+                        if (unit.dir===2 && unit.y + 1 < this.map.height) unit.y++;
+                        if (unit.dir===3 && unit.x>0) unit.x--;
+                        drawGrid();
+                    }
+                    log("Walk " + steps + " step(s)");
+                }
+            },
+            "Turn": { 
+                args: { direction: { type: ["left","right"], default: "left" } }, 
+                subWorkspace: false,
+                action: function  (cmd, unit, state) {
+                    if (cmd.args.direction==="left") {
+                        unit.dir = (unit.dir+3)%4;
+                        drawGrid();
+                        log("Turn left");
+                    } else if (cmd.args.direction==="right") {
+                        unit.dir = (unit.dir+1)%4;
+                        drawGrid();
+                        log("Turn right");
+                    }
+                }
+            },
+            "Identify": { 
+                args: { 
+                key: { type: "string", default: "x" }, 
+                operator: { type: ["<","<=",">=",">","==","!="], default: "==" }, 
+                value: { type: "string", default: "0" } 
+                }, 
+                subWorkspace: true ,
+                action: function (cmd, unit, state) {
+                    const key = cmd.args.key || "";
+                    const op = cmd.args.operator || "==";
+                    const val = cmd.args.value || "";
+                    log(`Identify: ${key} ${op} ${val}`);
+                    // For demo, always execute sub-workspace
+                    exec.call(this, cmd.workspace);
+                }
+            }
+        }
+
 export const levels = {
     level1: {
         name: "Level One",
@@ -23,77 +96,7 @@ export const levels = {
                 }
             ]
         },
-        schema : {
-            "Say": { 
-                args: { text: { type: "string", default: "Hello" } }, 
-                subWorkspace: false,
-                action: (cmd, unit, state) => {
-                    log("Say: " + (cmd.args.text || ""));
-                }
-            },
-            "Feel": { 
-                args: { space: { type: ["Empty","Wall","Stairs","Unit"], default: "Empty" } }, 
-                subWorkspace: true,
-                action: (cmd, unit, state) => {
-                    log("Feel: " + (cmd.args.space || ""));
-                    exec.call(this, cmd.workspace);
-                }
-            },
-            "Hear": { 
-                args: { phrase: { type: "string", default: "Hi" } }, 
-                subWorkspace: true,
-                action: function (cmd, unit, state) {
-                    log("Hear: " + (cmd.args.phrase || ""));
-                    exec.call(this, cmd.workspace);
-                }
-            },
-            "Walk": { 
-                args: { steps: { type: "number", default: 1 } }, 
-                subWorkspace: false,
-                action: function (cmd, unit, state) {
-                    let steps = parseInt(cmd.args.steps) || 0;
-                    for (let i=0; i<steps; i++) {
-                        if (unit.dir===0 && unit.y>0) unit.y--;
-                        if (unit.dir===1 && unit.x + 1 < this.map.width) unit.x++;
-                        if (unit.dir===2 && unit.y + 1 < this.map.height) unit.y++;
-                        if (unit.dir===3 && unit.x>0) unit.x--;
-                        drawGrid();
-                    }
-                    log("Walk " + steps + " step(s)");
-                }
-            },
-            "Turn": { 
-                args: { direction: { type: ["left","right"], default: "left" } }, 
-                subWorkspace: false,
-                action: function  (cmd, unit, state) {
-                    if (cmd.args.direction==="left") {
-                        unit.dir = (unit.dir+3)%4;
-                        drawGrid();
-                        log("Turn left");
-                    } else if (cmd.args.direction==="right") {
-                        unit.dir = (unit.dir+1)%4;
-                        drawGrid();
-                        log("Turn right");
-                    }
-                }
-            },
-            "Identify": { 
-                args: { 
-                key: { type: "string", default: "x" }, 
-                operator: { type: ["<","<=",">=",">","==","!="], default: "==" }, 
-                value: { type: "string", default: "0" } 
-                }, 
-                subWorkspace: true ,
-                action: function (cmd, unit, state) {
-                    const key = cmd.args.key || "";
-                    const op = cmd.args.operator || "==";
-                    const val = cmd.args.value || "";
-                    log(`Identify: ${key} ${op} ${val}`);
-                    // For demo, always execute sub-workspace
-                    exec.call(this, cmd.workspace);
-                }
-            }
-        }
+        schema
     },
     level2: {
         name: "Level Two",
@@ -115,77 +118,6 @@ export const levels = {
                 }
             ]
         },
-        schema : {
-            "Say": { 
-                args: { text: { type: "string", default: "Hello" } }, 
-                subWorkspace: false,
-                action: (cmd, unit, state) => {
-                    log("Say: " + (cmd.args.text || ""));
-                }
-            },
-            "Feel": { 
-                args: { space: { type: ["Empty","Wall","Stairs","Unit"], default: "Empty" } }, 
-                subWorkspace: true,
-                action: (cmd, unit, state) => {
-                    log("Feel: " + (cmd.args.space || ""));
-                    exec.call(this, cmd.workspace);
-                }
-            },
-            "Hear": { 
-                args: { phrase: { type: "string", default: "Hi" } }, 
-                subWorkspace: true,
-                action: function (cmd, unit, state) {
-                    log("Hear: " + (cmd.args.phrase || ""));
-                    exec.call(this, cmd.workspace);
-                }
-            },
-            "Walk": { 
-                args: { steps: { type: "number", default: 1 } }, 
-                subWorkspace: false,
-                action: async function (cmd, unit, state) {
-                    let steps = parseInt(cmd.args.steps) || 0;
-                    for (let i=0; i<steps; i++) {
-                        if (unit.dir===0 && unit.y>0) unit.y--;
-                        if (unit.dir===1 && unit.x + 1 < this.map.width) unit.x++;
-                        if (unit.dir===2 && unit.y + 1 < this.map.height) unit.y++;
-                        if (unit.dir===3 && unit.x>0) unit.x--;
-                        await sleep(333);
-                        drawGrid();
-                    }
-                    log("Walk " + steps + " step(s)");
-                }
-            },
-            "Turn": { 
-                args: { direction: { type: ["left","right"], default: "left" } }, 
-                subWorkspace: false,
-                action: function  (cmd, unit, state) {
-                    if (cmd.args.direction==="left") {
-                        unit.dir = (unit.dir+3)%4;
-                        drawGrid();
-                        log("Turn left");
-                    } else if (cmd.args.direction==="right") {
-                        unit.dir = (unit.dir+1)%4;
-                        drawGrid();
-                        log("Turn right");
-                    }
-                }
-            },
-            "Identify": { 
-                args: { 
-                key: { type: "string", default: "x" }, 
-                operator: { type: ["<","<=",">=",">","==","!="], default: "==" }, 
-                value: { type: "string", default: "0" } 
-                }, 
-                subWorkspace: true ,
-                action: function (cmd, unit, state) {
-                    const key = cmd.args.key || "";
-                    const op = cmd.args.operator || "==";
-                    const val = cmd.args.value || "";
-                    log(`Identify: ${key} ${op} ${val}`);
-                    // For demo, always execute sub-workspace
-                    exec.call(this, cmd.workspace);
-                }
-            }
-        }
+        schema
     }
 }
